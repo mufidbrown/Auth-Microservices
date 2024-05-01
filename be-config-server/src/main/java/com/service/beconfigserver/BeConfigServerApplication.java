23331146efa8e1f9da96a2d4627139d9aa0d7223
mufidbrown <ilhammufid67@gmail.com>
2024-05-02 01:31:26 +0700

    first commit to github

---
+++ be-config-server/src/main/java/com/service/beconfigserver/BeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.service.beconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeConfigServerApplication.class, args);
+	}
+
+}
