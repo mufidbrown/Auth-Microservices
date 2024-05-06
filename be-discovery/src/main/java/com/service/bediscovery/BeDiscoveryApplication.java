2a6e7aaed30631c17307ac7163f8103692aa79b1
mufidbrown <ilhammufid67@gmail.com>
2024-05-06 17:18:46 +0700

    add be-cover discovery

---
+++ be-discovery/src/main/java/com/service/bediscovery/BeDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.service.bediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeDiscoveryApplication.class, args);
+	}
+
+}
