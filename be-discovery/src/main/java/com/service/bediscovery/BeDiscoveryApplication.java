f2a0188641d1329229a3803aa24799459dd8b186
mufidbrown <ilhammufid67@gmail.com>
2024-05-10 06:39:57 +0700

    add configuration & setting server

--- be-discovery/src/main/java/com/service/bediscovery/BeDiscoveryApplication.java
+++ be-discovery/src/main/java/com/service/bediscovery/BeDiscoveryApplication.java
@@ -2,7 +2,10 @@ package com.service.bediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class BeDiscoveryApplication {
 
