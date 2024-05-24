e4fe891f309454da872f0118c0ecca5fd1554eb5
mufidbrown <ilhammufid67@gmail.com>
2024-05-25 05:58:25 +0700

    add catalog

---
+++ be-catalog/src/main/java/com/service/be_catalog/BeCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.service.be_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCatalogApplication.class, args);
+	}
+
+}
