56b98d02e735b0d489a475fe5f2a1292475c23e6
mufidbrown <ilhammufid67@gmail.com>
2024-05-25 03:47:30 +0700

    add gateway

---
+++ be-gateway/src/main/java/com/service/be_gateway/BeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.service.be_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGatewayApplication.class, args);
+	}
+
+}
